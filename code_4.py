3672c6e7e82c73f9ac2988fcffa18205ad144b33
fyhinojosa <147429973+fyhinojosa@users.noreply.github.com>
2024-02-15 13:21:00 -0700

    1

--- code_4.py
+++ code_4.py
@@ -1,18 +1,35 @@
 import csv
-import pandas as pd
+import pandas
 big_mac_file = './big-mac-full-index.csv'
 
+df = pandas.read_csv('big-mac-full-index.csv')
+
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    filter_df = df[(df['date']== year) & (df['iso_a3']== country_code.lower())]
+    mean_price = filter_df['dollar_price'].mean()
+    return round(mean_price,2)
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    filter_df = df[df['iso_a3'] == country_code]
+    mean_price = filter_df['dollar_price'].mean()
+    return round(mean_price,2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    filter_df = df[df['date'] == year]
+    min_price = int(filter_df['dollar_price'].min())
+    cheapest_row = min_price[filter_df['dollar_price'] == min_price['dollar_price'].iloc[0]]
+    return round(cheapest_row,2)
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    filter_df = df[df['date'] == year]
+    max_price = filter_df['dollar_price'].max()
+    expensive_row = filter_df[filter_df['dollar_price'] == max_price].iloc[0]
+    return expensive_row
+
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    print(get_big_mac_price_by_year())
+    print(get_big_mac_price_by_country())
+    print(get_the_cheapest_big_mac_price_by_year())
+    print(get_the_most_expensive_big_mac_price_by_year())
+
