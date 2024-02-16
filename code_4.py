05d2c4173119b75a7d3d567e2980c9ad0f21de2c
fyhinojosa <147429973+fyhinojosa@users.noreply.github.com>
2024-02-16 14:40:14 -0700

    10

--- code_4.py
+++ code_4.py
@@ -13,19 +13,16 @@ def get_big_mac_price_by_year(year,country_code):
     return round(mean_price_year,2), round(mean_price_country, 2)
 
 def get_big_mac_price_by_country(country_code):
-    filter_df = df[df['iso_a3'] == country_code]
+    filter_df = df[df['iso_a3'].str.lower() == country_code]
     mean_price = filter_df['dollar_price'].mean()
     return round(mean_price,2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
     filter_df = df[df['date'] == year]
-    if filter_df.empty:
-        return "No data available for the specified year."
-    else:
-        cheapest_row = filter_df.loc[filter_df['dollar_price'].idxmin()]
-        country_name = cheapest_row['name']
-        country_code = cheapest_row['iso_a3']
-        dollar_price = cheapest_row['dollar_price']
+    cheapest_row = filter_df.loc[filter_df['dollar_price'].idxmin()]
+    country_name = cheapest_row['name']
+    country_code = cheapest_row['iso_a3']
+    dollar_price = cheapest_row['dollar_price']
     return f"{country_name}({country_code}): ${round(dollar_price, 2)}"
 
 def get_the_most_expensive_big_mac_price_by_year(year):
@@ -40,8 +37,8 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     return f"{country_name}({country_code}): ${round(dollar_price, 2)}"
 
 if __name__ == "__main__":
-    print(get_big_mac_price_by_year(2000,'ARG'))
-    print(get_big_mac_price_by_country('IND'))
+    print(get_big_mac_price_by_year(2000,'bra'))
+    print(get_big_mac_price_by_country('ind'))
     print(get_the_cheapest_big_mac_price_by_year(2012))
     print(get_the_most_expensive_big_mac_price_by_year(2003))
 
